e1893c2cd1634d91bacda58499fa8cd88190c576
AdmiralBankir <46968473+AdmiralBankir@users.noreply.github.com>
2019-09-24 13:49:18 +0300

    Исправление замечаний

--- js/main.js
+++ js/main.js
@@ -2,12 +2,11 @@
 
 var AVATAR_SAMPLE = 'img/avatars/user';
 var TITLE = 'Заголовок предложения';
-var ADDRESS = '600, 350';
 var TYPE = ['palace', 'flat', 'house', 'bungalo'];
 var CHECK_IN_OR_OUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var DESC = 'Строка с описанием';
-var PHOTO_SAMPLE = '"http://o0.github.io/assets/images/tokyo/hotel';
+var PHOTO_SAMPLE = 'http://o0.github.io/assets/images/tokyo/hotel';
 var maxWidthMap = 600;
 var MIN_Y_MAP = 130;
 var MAX_Y_MAP = 160;
@@ -36,7 +35,7 @@ var createPhotosArray = function () {
   return photos;
 };
 
-var createMoch = function () {
+var createMochForPins = function () {
   var moch = [];
   for (var i = 0; i < 8; i++) {
     moch[i] = {
@@ -47,7 +46,7 @@ var createMoch = function () {
       offer: {
         title: TITLE,
 
-        address: ADDRESS,
+        address: String(location.x + ',' + location.y),
 
         price: randomInt(0, 1000),
 
@@ -77,16 +76,6 @@ var createMoch = function () {
   return moch;
 };
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
-
-maxWidthMap = map.offsetWidth;
-var moch = createMoch();
-
-var mapPins = document.querySelector('.map__pins');
-var mapPinMain = mapPins.querySelector('.map__pin--main');
-var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-
 var createPin = function (pinAttr) {
   var pin = pinTemplate.cloneNode(true);
   var img = pin.querySelector('img');
@@ -94,16 +83,30 @@ var createPin = function (pinAttr) {
   var pinWidth = img.width;
   var pinHeight = img.height;
 
-  pin.style = 'left:' + String(pinAttr.location.x - pinWidth / 2) + 'px;' + 'top:' + String(pinAttr.location.y - pinHeight) + 'px;';
+  var leftPosition = pinAttr.location.x - pinWidth / 2;
+  var topPosition = pinAttr.location.y - pinHeight;
+
+  pin.style = 'left:' + leftPosition + 'px;' + 'top:' + topPosition + 'px;';
   img.src = pinAttr.author.avatar;
   img.alt = pinAttr.offer.title;
 
   return pin;
 };
 
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+
+maxWidthMap = map.offsetWidth;
+var pins = createMochForPins();
+
+var mapPins = document.querySelector('.map__pins');
+var mapPinMain = mapPins.querySelector('.map__pin--main');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < moch.length; i++) {
-  fragment.appendChild(createPin(moch[i]));
+for (var i = 0; i < pins.length; i++) {
+  fragment.appendChild(createPin(pins[i]));
 }
 
 mapPinMain.appendChild(fragment);
