8da24ff4061554629f0dd0722617bb94ae7e93e3
AdmiralBankir <46968473+AdmiralBankir@users.noreply.github.com>
2019-09-27 18:25:02 +0300

    Без валидации формы

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ var PHOTO_SAMPLE = 'http://o0.github.io/assets/images/tokyo/hotel';
 var maxWidthMap = 600;
 var MIN_Y_MAP = 130;
 var MAX_Y_MAP = 630;
+var ENTER_KEYCODE = 13;
 
 var randomInt = function (min, max) {
   var rand = min + Math.random() * (max - min);
@@ -95,19 +96,90 @@ var createPin = function (pinAttr) {
   return pin;
 };
 
+var createPinsOnMap = function () {
+  maxWidthMap = map.offsetWidth;
+  var pins = createMochForPins();
+
+  var mapPins = document.querySelector('.map__pins');
+
+
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < pins.length; i++) {
+    fragment.appendChild(createPin(pins[i]));
+  }
+
+  mapPins.appendChild(fragment);
+};
+
+var getAddressFromPin = function () {
+  var xCoordPin = mapPinMain.offsetLeft;
+  var yCoordPin = mapPinMain.offsetTop;
+
+  var xCoordAddress = xCoordPin + 0.5 * mapPinMain.offsetWidth;
+  var yCoordAddress;
+
+  if (map.classList.contains('map--faded')) {
+    yCoordAddress = yCoordPin + 0.5 * mapPinMain.offsetWidth;
+  } else {
+    yCoordAddress = yCoordPin + mapPinMain.offsetWidth;
+  }
+
+  yCoordAddress = (yCoordAddress > MAX_Y_MAP) ? MAX_Y_MAP : yCoordAddress;
+  yCoordAddress = (yCoordAddress < MIN_Y_MAP) ? MIN_Y_MAP : yCoordAddress;
+
+  xCoordAddress = (xCoordAddress > map.offsetWidth) ? map.offsetWidth : xCoordAddress;
+
+  return String(Math.floor(xCoordAddress) + ',' + ' ' + Math.floor(yCoordAddress));
+};
+
+var setAddress = function () {
+  var addressInput = adForm.querySelector('#address');
+  addressInput.value = getAddressFromPin();
+};
+
+var setPageState = function (state) {
+
+  if (state) {
+    map.classList.remove('map--faded');
+
+    adForm.classList.remove('ad-form--disabled');
+
+    for (var i = 0; i < adFormFieldsets.length; i++) {
+      adFormFieldsets[i].disabled = false;
+    }
+
+    mapFilters.removeAttribute('disabled');
+
+  } else {
+    map.classList.add('map--faded');
+
+    adForm.classList.add('ad-form--disabled');
+
+    for (i = 0; i < adFormFieldsets.length; i++) {
+      adFormFieldsets[i].disabled = true;
+    }
+
+    mapFilters.setAttribute('disabled', 'disabled');
+  }
+
+  setAddress();
+};
+
 var map = document.querySelector('.map');
-map.classList.remove('map--faded');
-
-maxWidthMap = map.offsetWidth;
-var pins = createMochForPins();
-
-var mapPins = document.querySelector('.map__pins');
+var adForm = document.querySelector('.ad-form');
+var adFormFieldsets = adForm.querySelectorAll('fieldset');
+var mapFilters = document.querySelector('.map__filters');
+var mapPinMain = map.querySelector('.map__pin--main');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
+setPageState(false);
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < pins.length; i++) {
-  fragment.appendChild(createPin(pins[i]));
-}
+mapPinMain.addEventListener('mousedown', function () {
+  setPageState(true);
+});
 
-mapPins.appendChild(fragment);
+mapPinMain.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    setPageState(true);
+  }
+});
