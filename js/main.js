012c7f9c122a7f57fedc99fa21656ca3f4f1c542
AdmiralBankir <46968473+AdmiralBankir@users.noreply.github.com>
2019-10-02 19:57:03 +0300

    Активация. Валидация

--- js/main.js
+++ js/main.js
@@ -165,12 +165,110 @@ var setPageState = function (state) {
   setAddress();
 };
 
+var setMinPrice = function (select) {
+  var minPriceInput = adForm.querySelector('#price');
+  var minPrice;
+  var currentOption = select.options[select.selectedIndex];
+  switch (currentOption.value) {
+    case 'bungalo':
+      minPrice = 0;
+      break;
+
+    case 'flat':
+      minPrice = 1000;
+      break;
+
+    case 'house':
+      minPrice = 5000;
+      break;
+
+    case 'palace':
+      minPrice = 10000;
+      break;
+
+    default:
+      break;
+  }
+
+  minPriceInput.min = minPrice;
+  minPriceInput.placeholder = minPrice;
+
+};
+
+var createInputValidity = function () {
+  for (var i = 0; i < inputs.length; i++) {
+    var input = inputs[i];
+    input.addEventListener('invalid', function () {
+    });
+    if (input.id === 'address') {
+      input.setAttribute('disabled', 'disabled');
+    }
+  }
+};
+
+var onchangeSelect = function (evt) {
+  var modifiedSelect = evt.target;
+  var modifiedOption = modifiedSelect.options[modifiedSelect.selectedIndex];
+  var modifiedValue = modifiedOption.value;
+
+  var selectsArr = adFormElementTime.querySelectorAll('select');
+
+  for (var i = 0; i < selectsArr.length; i++) {
+    if (selectsArr[i].id !== modifiedSelect.id) {
+      selectsArr[i].value = modifiedValue;
+      break;
+    }
+  }
+};
+
+var onchangeRoomNumber = function () {
+  var currentOption = roomNumber.options[roomNumber.selectedIndex];
+  var capacityOfRoom = adForm.querySelector('#capacity');
+  var capacityOptions = capacityOfRoom.querySelectorAll('option');
+
+  var masksAvaliableCapacity = [];
+
+  switch (currentOption.value) {
+    case '1':
+      masksAvaliableCapacity = [1];
+      break;
+
+    case '2':
+      masksAvaliableCapacity = [1, 2];
+      break;
+
+    case '3':
+      masksAvaliableCapacity = [1, 2, 3];
+      break;
+
+    case '100':
+      masksAvaliableCapacity = [0];
+      break;
+
+    default:
+      break;
+  }
+
+  for (var i = 0; i < capacityOptions.length; i++) {
+    capacityOptions[i].setAttribute('disabled', 'disabled');
+    if (masksAvaliableCapacity.indexOf(Number(capacityOptions[i].value)) !== -1) {
+      capacityOptions[i].removeAttribute('disabled');
+    }
+  }
+};
+
+
 var map = document.querySelector('.map');
 var adForm = document.querySelector('.ad-form');
 var adFormFieldsets = adForm.querySelectorAll('fieldset');
 var mapFilters = document.querySelector('.map__filters');
 var mapPinMain = map.querySelector('.map__pin--main');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var inputs = adForm.querySelectorAll('input');
+var select = adForm.querySelector('#type');
+var adFormElementTime = adForm.querySelector('.ad-form__element--time');
+var roomNumber = adForm.querySelector('#room_number');
+
 
 setPageState(false);
 
@@ -183,3 +281,13 @@ mapPinMain.addEventListener('keydown', function (evt) {
     setPageState(true);
   }
 });
+
+select.addEventListener('change', function () {
+  setMinPrice(select);
+});
+
+adFormElementTime.addEventListener('change', onchangeSelect);
+
+roomNumber.addEventListener('change', onchangeRoomNumber);
+
+createInputValidity();
