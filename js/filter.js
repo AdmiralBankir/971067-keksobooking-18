ed2cb335439ec2954836abfc7d444f6012312f30
AdmiralBankir <46968473+AdmiralBankir@users.noreply.github.com>
2019-10-21 18:21:31 +0300

    Исправление замечаний

--- js/filter.js
+++ js/filter.js
@@ -7,91 +7,89 @@
   var housingRooms = filterForm.querySelector('#housing-rooms');
   var housingGuests = filterForm.querySelector('#housing-guests');
   var housingFeatures = filterForm.querySelector('#housing-features').querySelectorAll('input');
+  var filterOption;
 
-  function Filter() {
-    this.type = '';
-    this.price = '';
-    this.rooms = '';
-    this.guests = '';
-    this.features = [];
+  function Filter(value, handler) {
+    this.value = value;
+    this.handler = handler;
   }
 
-  var filterOption = new Filter();
+  function FilterCollection() {
+    this.type = new Filter('', isMatchingOffer);
+    this.price = new Filter('', isMatchingPrice);
+    this.rooms = new Filter('', isMatchingOffer);
+    this.guests = new Filter('', isMatchingOffer);
+    this.features = new Filter([], isMatchingFeatures);
+  }
+
+  var priceList = {
+    low: {min: 0, max: 9999},
+    middle: {min: 10000, max: 50000},
+    high: {min: 50000, max: Infinity}
+  };
 
   var getFilterOption = function () {
-    filterOption = new Filter();
-    filterOption.type = housingType.options[housingType.selectedIndex].value;
-    filterOption.price = housingPrice.options[housingPrice.selectedIndex].value;
-    filterOption.rooms = housingRooms.options[housingRooms.selectedIndex].value;
-    filterOption.guests = housingGuests.options[housingGuests.selectedIndex].value;
+    filterOption = new FilterCollection();
+    filterOption.type.value = housingType.value;
+    filterOption.price.value = housingPrice.value;
+    filterOption.rooms.value = housingRooms.value;
+    filterOption.guests.value = housingGuests.value;
 
-    housingFeatures.forEach(function (elem) {
-      if (elem.checked) {
-        filterOption.features.push(elem.value);
-      }
+    var features = Array.from(housingFeatures).filter(function (checkbox) {
+      return checkbox.checked;
     });
 
+    features.forEach(function (element) {
+      filterOption.features.value.push(element.value);
+    });
 
     var keys = Object.keys(filterOption);
 
     keys.forEach(function (elem) {
 
-      if (elem === 'guests' || elem === 'rooms') {
-        filterOption[elem] = Number(filterOption[elem]);
-        if (isNaN(filterOption[elem])) {
-          delete filterOption[elem];
-        }
-      } else if (filterOption[elem] === 'any' ||
-        filterOption[elem].length === 0) {
+      if (filterOption[elem].value === 'any') {
         delete filterOption[elem];
       }
 
     });
+
+    if (filterOption.features.value.length === 0) {
+      delete filterOption.features;
+    }
   };
 
   var isMatchingPrice = function (pin) {
-    var range = [];
-    var matching = false;
-    switch (filterOption.price) {
-      case 'middle':
-        range = [10000, 50000];
-        break;
-      case 'low':
-        range = [0, 9999];
-        break;
-      case 'high':
-        range = [50000, Infinity];
-        break;
-    }
-
-    if (pin.offer.price <= range[1] && pin.offer.price >= range[0]) {
-      matching = true;
-    }
-
-    return matching;
+    var range = priceList[filterOption.price.value];
+    return (pin.offer.price <= range.max && pin.offer.price >= range.min);
   };
 
   var isMatchingFeatures = function (pin) {
-    return (JSON.stringify(pin.offer.features.slice().sort()) ===
-    JSON.stringify(filterOption.features.slice().sort()));
+    return (filterOption.features.value.every(function (element) {
+      return (pin.offer.features.indexOf(element) + 1);
+    }));
+  };
+
+  var isMatchingOffer = function (pin, option) {
+    var value = filterOption[option].value;
+    if (!isNaN(Number(value))) {
+      value = Number(value);
+    }
+    return (pin.offer[option] === value);
   };
 
   var isMatchingPin = function (pin) {
     var activeOptions = Object.keys(filterOption);
-    var matching = true;
+    var matching = 0;
 
     activeOptions.forEach(function (option) {
-      if (pin.offer[option] !== filterOption[option]) {
-        matching = false;
-        if (option === 'price') {
-          matching = isMatchingPrice(pin);
-        } else if (option === 'features') {
-          matching = isMatchingFeatures(pin);
-        }
-      }
+      matching += filterOption[option].handler(pin, option);
     });
 
-    return matching;
+    if (matching !== activeOptions.length) {
+      return false;
+    }
+
+    return true;
   };
 
 
--- js/pins-ctrl.js
+++ js/pins-ctrl.js
@@ -1,62 +1,66 @@
 'use strict';
-var map = document.querySelector('.map');
-window.dataPins = [];
 
-var createPin = function (pinAttr) {
-  var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-  var pin = pinTemplate.cloneNode(true);
-  var img = pin.querySelector('img');
+(function () {
+  var map = document.querySelector('.map');
+  window.dataPins = [];
+  var numberOfPins = 5;
 
-  var pinWidth = img.width;
-  var pinHeight = img.height;
+  var createPin = function (pinAttr) {
+    var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+    var pin = pinTemplate.cloneNode(true);
+    var img = pin.querySelector('img');
 
-  var leftPosition = pinAttr.location.x - pinWidth / 2;
-  var topPosition = pinAttr.location.y - pinHeight;
+    var pinWidth = img.width;
+    var pinHeight = img.height;
 
-  pin.style.left = leftPosition + 'px';
-  pin.style.top = topPosition + 'px';
-  img.src = pinAttr.author.avatar;
-  img.alt = pinAttr.offer.title;
+    var leftPosition = pinAttr.location.x - pinWidth / 2;
+    var topPosition = pinAttr.location.y - pinHeight;
 
-  return pin;
-};
+    pin.style.left = leftPosition + 'px';
+    pin.style.top = topPosition + 'px';
+    img.src = pinAttr.author.avatar;
+    img.alt = pinAttr.offer.title;
 
-window.renderPins = function (pins) {
-  if (pins.length !== 0) {
-    var lenPin = (pins.length > 5) ? 5 : pins.length;
-    var mapPins = document.querySelector('.map__pins');
-    var fragment = document.createDocumentFragment();
-    for (var i = 0; i < lenPin; i++) {
-      fragment.appendChild(createPin(pins[i]));
+    return pin;
+  };
+
+  window.renderPins = function (pins) {
+    if (pins.length !== 0) {
+      var lenPin = (pins.length > numberOfPins) ? numberOfPins : pins.length;
+      var mapPins = document.querySelector('.map__pins');
+      var fragment = document.createDocumentFragment();
+      for (var i = 0; i < lenPin; i++) {
+        fragment.appendChild(createPin(pins[i]));
+      }
+      mapPins.appendChild(fragment);
+    } else {
+      window.ctrlPins.removePinsOnMap();
     }
-    mapPins.appendChild(fragment);
-  } else {
-    window.ctrlPins.removePinsOnMap();
-  }
-};
+  };
 
-window.ctrlPins = {
-  createPinsOnMap: function () {
-    window.sendRequest(null, 'load', function (pins) {
-      window.dataPins = pins.slice();
-      window.renderPins(pins);
-    });
-  },
+  window.ctrlPins = {
+    createPinsOnMap: function () {
+      window.sendRequest(null, 'load', function (pins) {
+        window.dataPins = pins.slice();
+        window.renderPins(pins);
+      });
+    },
 
-  removePinsOnMap: function () {
-    var pins = map.querySelectorAll('.map__pin:not(.map__pin--main)');
-    for (var i = pins.length - 1; i >= 0; i--) {
-      var pin = pins[i];
-      pin.parentElement.removeChild(pin);
+    removePinsOnMap: function () {
+      var pins = map.querySelectorAll('.map__pin:not(.map__pin--main)');
+      for (var i = pins.length - 1; i >= 0; i--) {
+        var pin = pins[i];
+        pin.parentElement.removeChild(pin);
+      }
+    },
+
+    resetMainPin: function () {
+      var INIT_LEFT_MAIN_PIN = 570;
+      var INIT_TOP_MAIN_PIN = 375;
+      var pinMain = map.querySelector('.map__pin--main');
+      pinMain.style.left = INIT_LEFT_MAIN_PIN + 'px';
+      pinMain.style.top = INIT_TOP_MAIN_PIN + 'px';
+      window.formCtrl.setAddress();
     }
-  },
-
-  resetMainPin: function () {
-    var INIT_LEFT_MAIN_PIN = 570;
-    var INIT_TOP_MAIN_PIN = 375;
-    var pinMain = map.querySelector('.map__pin--main');
-    pinMain.style.left = INIT_LEFT_MAIN_PIN + 'px';
-    pinMain.style.top = INIT_TOP_MAIN_PIN + 'px';
-    window.formCtrl.setAddress();
-  }
-};
+  };
+})();
